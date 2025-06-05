07fe80da2ce2eb8e1ab9a9eab958a6b8a9150d54
Nguyen Van Tien <ngvantien1526@gmail.com>
2025-06-05 16:22:04 +0700

    add solution solve by c++: No.1635

---
+++ cses/1635. Coin Combinations I/Coin_Combinations_I.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int main() {
+    ios_base :: sync_with_stdio(0);
+    cin.tie(0);  cout.tie(0);
+
+    int n, x;
+    cin >> n >> x;
+
+    vector <long long> coins(n), dp(x+1);
+
+    for (auto &coin : coins) cin >> coin;
+    dp[0] = 1;
+
+    for (int i = 1; i <= x; ++i) {
+        for (auto &coin : coins) {
+            if (i - coin >= 0) {
+                dp[i] = (dp[i] + dp[i-coin]) % MOD;
+            }
+        }
+    }
+
+    cout << dp[x] % MOD << '\n';
+    return 0;
+}
