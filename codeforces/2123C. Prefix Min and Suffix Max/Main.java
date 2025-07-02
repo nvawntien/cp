69d8396de977576ecb54e1799758aef5e5d6000c
Nguyen Van Tien <ngvantien1526@gmail.com>
2025-07-02 21:05:33 +0700

    update solution

--- codeforces/2123C. Prefix Min and Suffix Max/Main.java
+++ codeforces/2123C. Prefix Min and Suffix Max/Main.java
@@ -1,15 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    static void solve(Scanner in) {
-        int n = in.nextInt();
+    static void solve() {
+        int n = reader.nextInt();
         int arr[] = new int[n+1];
 
         for (int i = 1; i <= n; i++) {
-            arr[i] = in.nextInt();
+            arr[i] = reader.nextInt();
         }
 
-        int pre[] = new int[n+1], suf[] = new int[n+2];
+        int pre[] = new int[n+1];
+        int suf[] = new int[n+2];
+
         pre[0] = arr[1];
         suf[n+1] = arr[n];
 
@@ -23,23 +29,66 @@ public class Main {
 
         for (int i = 1; i <= n; i++) {
             if (arr[i] == pre[i] || arr[i] == suf[i]) {
-                System.out.print("1");
-            } else {
-                System.out.print("0");
+                writer.print("1");
+            } 
+            else {
+                writer.print("0");
             }
         }
 
-        System.out.println();
+        writer.println();
+    }
+    
+    public static void main(String[] args) {
+        int T = reader.nextInt();
+        for (int i = 0; i < T; i++) solve();
+        writer.flush();
     }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int T = in.nextInt();
+    static FastReader reader = new FastReader();
+    static PrintWriter writer = new PrintWriter(System.out);
 
-        while (T-- > 0) {
-            solve(in);
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        in.close();
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return str;
+        }
     }
 }
