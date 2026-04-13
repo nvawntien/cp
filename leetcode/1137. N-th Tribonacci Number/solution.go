888972904c6f97609995a0caecd9a9ec771f232e
nvawntien <ngvantiendev@gmail.com>
2026-04-13 18:13:54 +0700

    add solution solve by golang: No.1137

---
+++ leetcode/1137. N-th Tribonacci Number/solution.go
@@ -0,0 +1,12 @@
+func tribonacci(n int) int {
+    f := make([]int, 40)
+    f[0] = 0
+    f[1] = 1
+    f[2] = 1
+
+    for i := 3; i <= n; i++ {
+        f[i] = f[i-1] + f[i-2] + f[i-3]
+    }
+
+    return f[n]
+}
