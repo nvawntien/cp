b4a6c3e380fcc59102c47d745952b12b722e04a3
nvawntien <ngvantiendev@gmail.com>
2026-04-17 22:05:21 +0700

    add solution solve by golang: No.657

---
+++ leetcode/657. Robot Return to Origin/solution.go
@@ -0,0 +1,17 @@
+func judgeCircle(moves string) bool {
+    x, y := 0, 0
+    for i := range moves {
+        switch moves[i] {
+            case 'U':
+                y++
+            case 'D':
+                y--
+            case 'L':
+                x--
+            default:
+                x++
+        }
+    }
+
+    return x == 0 && y == 0
+}
