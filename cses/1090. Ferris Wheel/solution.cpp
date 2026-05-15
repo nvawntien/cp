f8cd5f3644c03a04554dbb75ce769e71bdb20482
nvawntien <ngvantien1526@gmail.com>
2026-05-15 17:34:42 +0700

    add solution solve by cpp: No.1090

---
+++ cses/1090. Ferris Wheel/solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector <int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+    int i = 0, j = n - 1, count = 0;
+    while (i <= j) {
+        if (a[i] + a[j] <= x) {
+            i++;
+            j--;
+        } else {
+            j--;
+        }
+        count++;
+    }
+
+    cout << count << endl;
+    return 0;    
+}
