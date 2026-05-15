62415c95df82e1d9790f690378d1b7995f429e99
nvawntien <ngvantien1526@gmail.com>
2026-05-15 17:22:49 +0700

    add solution solve by cpp: No.1084

---
+++ cses/1084. Apartments/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    
+    vector <int> a(n), b(m);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    int i = 0, j = 0, count = 0;
+    while (i < n && j < m) {
+        if (abs(a[i] - b[j]) <= k) {
+            count++;
+            i++;
+            j++;
+        } else if (a[i] < b[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
